1d36983864d26c94cfdcdab1fe24e3c581d85852
=rachednemr <=rached.nemr@esprit.tn>
2013-10-18 01:42:18 +0100

    commi initial

---
+++ ejbModule/SheepFarmingManagementEJB/persistances/entities/shepheard.java
@@ -0,0 +1,13 @@
+package SheepFarmingManagementEJB.persistances.entities;
+
+public class shepheard {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
